9551790ac2276e8a1b783f11c72f00fc4e72d73d
vinayrk77 <vinayrk26@gmail.com>
2026-04-20 16:56:00 +0530

    Practice 20th Apr

---
+++ Interviwe_Practice/Fibonaccic_Number.java
@@ -0,0 +1,16 @@
+package Interviwe_Practice;
+
+public class Fibonaccic_Number {
+    public static void main(String[] args){
+        int n1 = 0;
+        int n2 = 1;
+        int sum = 0;
+        System.out.print(n1+" "+n2);
+        for (int i = 2; i<15; i++){
+            sum = n1+n2;
+            System.out.print(" "+sum);
+            n1=n2;
+            n2=sum;
+        }
+    }
+}
---
+++ Interviwe_Practice/Prime_Number.java
@@ -0,0 +1,26 @@
+package Interviwe_Practice;
+
+public class Prime_Number {
+    public static void main(String[] args){
+        int num = 11;
+        int count = 0;
+        if(num>1)
+        {
+            for(int i=1; i<=num; i++)
+            {
+                if(num%i == 0)
+                    count++;
+            }
+            if(count == 2)
+            {
+                System.out.println("Prime number");
+            }else{
+                System.out.println("Not a prime number");
+            }
+
+        }
+        else{
+            System.out.println("Not a Prime Number");
+        }
+    }
+}
